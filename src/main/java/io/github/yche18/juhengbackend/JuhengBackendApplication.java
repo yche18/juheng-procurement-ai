c0fd620736945488bdbab4c9445cd5fc5bb56b4e
Yi Chen <yche1008@student.monash.edu>
2026-09-21 13:12:58 +1000

    chore: initialize Juheng backend

---
+++ src/main/java/io/github/yche18/juhengbackend/JuhengBackendApplication.java
@@ -0,0 +1,15 @@
+package io.github.yche18.juhengbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuhengBackendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JuhengBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/yche18/juhengbackend/JuhengBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.yche18.juhengbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuhengBackendApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
